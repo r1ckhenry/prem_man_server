68e1e558df9a730f5103b20e35f18ed8319a35ac
Rick Henry <rick.henry@codeclan.com>
2016-05-08 20:08:48 +0100

    updates

--- app/controllers/nations_controller.rb
+++ app/controllers/nations_controller.rb
@@ -2,7 +2,14 @@ class NationsController < ApplicationController
 
   def index
     nations = Nation.all()
-    render( json: nations.to_json( :include => :clubs ) )
+
+    if params[:leagues] == "true" && params[:clubs] == "true"
+      render( json: nations.to_json( :include => { :leagues => { :include => :clubs }}))
+    elsif params[:leagues] == "true"
+      render( json: nations.to_json( :include => :leagues ) )
+    else
+      render( json: nations )
+    end
   end
 
   def show
